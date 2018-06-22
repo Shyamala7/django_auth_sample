eba0808f118c2015a3b784c5f22b3917d1fd17e2
Shyamala <shyamala@neunets.com>
2018-06-22 16:55:12 +0530

    Sample authentication in django rest

---
+++ test.py
@@ -0,0 +1,67 @@
+ 
+import sys
+
+try:
+    from django.conf import settings
+    from django.test.utils import get_runner
+    from django.core.management import call_command
+
+    settings.configure(
+        DEBUG=True,
+        USE_TZ=True,
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        ROOT_URLCONF='django_auth0.urls',
+        INSTALLED_APPS=[
+            'django.contrib.auth',
+            'django.contrib.sessions',
+            'django.contrib.contenttypes',
+            'django.contrib.sites',
+            'django_auth0'
+        ],
+        SITE_ID=1,
+        MIDDLEWARE_CLASSES=(),
+        AUTH0_CLIENT_ID='client id',
+        AUTH0_SECRET='secret',
+        AUTH0_DOMAIN='domain',
+        AUTH0_CALLBACK_URL='auth_callback',
+        AUTH0_SUCCESS_URL='/'
+    )
+
+    try:
+        import django
+        setup = django.setup
+    except AttributeError:
+        pass
+    else:
+        setup()
+
+    call_command('migrate')
+
+except ImportError:
+    import traceback
+    traceback.print_exc()
+    raise ImportError('To fix this error, run: pip install'
+                      ' -r requirements-test.txt')
+
+
+def run_tests(*test_args):
+    if not test_args:
+        test_args = ['tests']
+
+    # Run tests
+    runner = get_runner(settings)
+    test_runner = runner()
+
+    failures = test_runner.run_tests(test_args)
+
+    if failures:
+        sys.exit(bool(failures))
+
+
+if __name__ == '__main__':
+    run_tests(*sys.argv[1:])
